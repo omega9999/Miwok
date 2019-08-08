1b862875f313ab2c9fde8168581cc393f376d329
Claudio <auron.tidus@gmail.com>
2019-08-08 07:32:51 +0200

    dynamic color on row number

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +21,7 @@ import java.util.List;
 public class WordAdapter extends ArrayAdapter<Word> {
     public WordAdapter(@NonNull final Context context, @NonNull final List<Word> objects, final int backgroundColorId) {
         super(context, LIST_ITEM, objects);
-         // ContextCompat.getColor()
+        // ContextCompat.getColor()
         this.mBackgroundColor = context.getResources().getColor(backgroundColorId);
     }
 
@@ -43,7 +44,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
         if (word != null) {
             defaultWord.setText(word.getDefaultTranslation());
             miwokWord.setText(word.getMiwokTranslation());
-            if (word.hasImage()){
+            if (word.hasImage()) {
                 imageView.setVisibility(View.VISIBLE);
                 imageView.setImageResource(word.getImageResourceId());
             }
@@ -51,20 +52,30 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
 
         /*
-        * put a dark rectangle behind the view and set the view's opacity. This saves painting the rectangle when the view is 100% opaque.
-        * imageView.setColorFilter(Color.rgb(123, 123, 123), android.graphics.PorterDuff.Mode.MULTIPLY);
-        * view.getBackground().setColorFilter(color, PorterDuff.Mode.DARKEN);
-        */
-        if (Boolean.FALSE) {// colori delle righe con varianti chiare/scure
-            ViewGroup viewGroup = view.findViewById(R.id.text_container);
-            PorterDuff.Mode mode = position % 2 == 0 ? PorterDuff.Mode.DARKEN : PorterDuff.Mode.LIGHTEN;
-            viewGroup.getBackground().setColorFilter(Color.argb(80, 0, 0, 0), mode);
+         * put a dark rectangle behind the view and set the view's opacity. This saves painting the rectangle when the view is 100% opaque.
+         * imageView.setColorFilter(Color.rgb(123, 123, 123), android.graphics.PorterDuff.Mode.MULTIPLY);
+         * view.getBackground().setColorFilter(color, PorterDuff.Mode.DARKEN);
+         */
+        ViewGroup viewGroup = view.findViewById(R.id.text_container);
+        Drawable drawable = viewGroup.getBackground();
+        if (drawable != null) {
+            final int alpha = 40;
+            switch (position % 3) {
+                case 0:
+                    drawable.setColorFilter(Color.argb(alpha, 0, 0, 0), PorterDuff.Mode.DARKEN);
+                    break;
+                case 1:
+                    drawable.setColorFilter(Color.argb(alpha, 255, 255, 255), PorterDuff.Mode.LIGHTEN);
+                    break;
+                case 2:
+                    break;
+            }
         }
 
         return view;
     }
 
-    private void clearView(@NonNull final View view){
+    private void clearView(@NonNull final View view) {
         TextView miwokWord = view.findViewById(R.id.miwok_text_view);
         TextView defaultWord = view.findViewById(R.id.default_text_view);
         ImageView imageView = view.findViewById(R.id.image);
